79a9e4eb213e2783a03e80ac530348d413aa812f
michelmi67 <michel.lechevrel@gmail.com>
2021-10-13 10:51:44 +0200

    CSS

--- projet 4/views/include/header.php
+++ projet 4/views/include/header.php
@@ -1,20 +1,26 @@
 <header>
+    <div class = "container-fluid">
         <div class = "connexion">
-            <a href = "?action=connexion" class = "button" id = "button_connexion">connexion admin</a>
+            <a href = "?action=connexion" class = "btn btn-light" id = "button_connexion">connexion admin</a>
             <?php
             if(isset($_SESSION['id']))
             {
                 ?>
-                <a href = "?action=creer_billet" class = "button">Creation billet</a>
-                <a href = "?action=recup_article" class = "button">Interface article</a>
-                <a href = "?action=recup_commentaire" class = "button">Interface commentaire</a>
-                <a href = "?action=deconnexion" class = "button" id = "button_deconnexion">Deconnexion</a>
+                <div class = "row">
+                    <a href = "?action=creer_billet" class = "btn btn-light">Creation billet</a>
+                    <a href = "?action=recup_article" class = "btn btn-light">Interface article</a>
+                    <a href = "?action=recup_commentaire" class = "btn btn-light">Interface commentaire</a>
+                    <a href = "?action=deconnexion" class = "btn btn-light" id = "button_deconnexion">Deconnexion</a>
+                </div>
                 <?php
             }
             ?>
         </div>
         <div class = "article">
-            <a href = "?action=accueil" class = "button">Accueil</a>
-            <a href = "?action=index_article" class = "button">Articles</a>
+            <div class = "row">
+                <a href = "?action=accueil" class = "btn btn-light">Accueil</a>
+                <a href = "?action=index_article" class = "btn btn-light">Articles</a>
+            </div>
         </div>
-    </header>
+    </div>
+</header>
