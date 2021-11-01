f0d6268bf1075650387dff0ea2fc6b0961d6a8af
michelmi67 <michel.lechevrel@gmail.com>
2021-11-01 10:12:26 +0100

    modification des nav

--- projet 5/public/views/accueil.php
+++ projet 5/public/views/accueil.php
@@ -5,8 +5,11 @@
         <?php require('include/head.html'); ?>
     </head>
     <!--inclusion du header-->
-    <?php require('include/header.html'); ?>
-    <!--inclusion de la barre de navigation-->
-    <?php require('include/nav.html'); ?>
+    <?php require('include/header.html'); 
+    //inclusion de la barre de navigation gauche
+     require('include/nav_left.html'); 
+    //inclusion de la barre de navigation
+    require('include/nav.html');
+    ?>
     
 </html>
