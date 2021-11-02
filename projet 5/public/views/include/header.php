c0c3d09f6e88eac8ba6bce1709d93356bedb3283
michelmi67 <michel.lechevrel@gmail.com>
2021-11-02 10:05:41 +0100

    remplacement fichier html par php

---
+++ projet 5/public/views/include/header.php
@@ -0,0 +1,14 @@
+<div class = "logo">
+    <a href = "?action=accueil"><img src = "image/logo.png" alt = "logo du site"></a>
+    <h1>Social Kids</h1>
+    <div class = "btn_connexion">
+        <ul><li><a href = "?action=connexion">Connecte-toi !</a>
+                <a href = "?action=connexion"><img src = "image/connexion.jpg" alt = "image connexion"></a></li>
+                <ul>
+                    <li class = "menu_li"><a href = "?action=profil">Profil</a></li>
+                    <li class = "menu_li"><a href = "?action=deconnexion">déconnexion</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
