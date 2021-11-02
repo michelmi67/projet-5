56e7fd7b67e8b0dbbfefd4800be7dbd3078cf1e9
michelmi67 <michel.lechevrel@gmail.com>
2021-11-02 10:07:18 +0100

    ajout message erreur

--- projet 5/public/views/connexion.php
+++ projet 5/public/views/connexion.php
@@ -1,20 +1,25 @@
 <!DOCTYPE HTML>
 <html lang = "fr">
     <head>
-        <?php include('include/head.html')?>
+        <?php include('include/head.php')?>
     </head>
-    <body class = "connexion_enfant">
+    <body class = "connexion">
         <!--Inclusion du header -->
-        <?php include('include/header.html'); ?>    
+        <?php include('include/header.php'); ?>    
         <h1>connexion</h1>
         <!--Formulaire d'inscription pour un admin -->
         <form method = "post" action = "?action=connexion">
             <p>
-                <label for = "pseudo">Pseudo </label><input type = "pseudo" name = "pseudo" id = "pseudo" required/><br><br>
+                <label for = "pseudo_connexion">Pseudo </label><input type = "text" name = "pseudo_connexion" id = "pseudo_connexion" required/><br><br>
                 <label for = "pass_connexion">Mot de passe </label><input type = "password" name = "pass_connexion" id = "pass_connexion" required/><br><br>
                 <button class="btn btn-secondary" type = "submit" value = "envoyé">envoyer</button>
             </p>
         </form>
+        <?php 
+        if(isset($message_erreur)){
+            echo $message_erreur;
+        }
+        ?>
         <article>
             <p>Si tu n'es pas encore inscrit, demande à un de tes parents ou un représentant légale de faire l'inscription avec toi <a href ="?action=inscription">ici</a> !</p>
         </article>
