ad878293cc2ff9bbc5e08579ce74423f3b549160
michelmi67 <michel.lechevrel@gmail.com>
2021-11-04 19:36:57 +0100

    changement de nom

--- projet 5/public/views/inscription.php
+++ projet 5/public/views/inscription.php
@@ -9,14 +9,14 @@
     <body class = "inscription">
         <h1>Inscription</h1>
         <form method = "post" action = "#">
-            <label for = "date_naissance">Date de naissance du responsable légal</label><input type = "date" name = "date_naissance" id = "date_naissance" required><br>
-            <label for = nom_enfant>Nom de l'enfant</label><input type = "text" name = "nom_enfant" id = "nom_enfant" required><br><label for = "prenom_enfant">Prénom de l'enfant</label>
-            <input type = "text" name = "prenom_enfant" id = "prenom_enfant" required><br>
-            <label for = "date_naissance_enfant">Date de naissance de l'enfant</label><input type = "date" name = "date_naissance_enfant" id = "date_naissance_enfant" required><br>
-            <label for = "pseudo_enfant">Pseudo de l'enfant</label><input type = "text" name = "pseudo_enfant" id = "pseudo_enfant" required><br>
-            <label for = "email_parent">Email du représentant légal</label><input type = "email" name = "email_parent" id = "email_parent" required><br>
-            <label for = "pass_enfant">Mot de passe </label><input type = "password" name = "pass_enfant" id = "pass_enfant" required/><br>
-            <label for = "pass_verification_enfant">Vérification du mot de passe </label><input type = "password" name = "pass_verification_enfant" id = "pass_verification_enfant" required/><br>
+            <label for = "date_naissance">Date de naissance du responsable légal</label><input type = "date" name = "date_naissance" id = "date_naissance"><br>
+            <label for = nom>Nom</label><input type = "text" name = "nom" id = "nom" required><br>
+            <label for = "prenom">Prénom</label><input type = "text" name = "prenom" id = "prenom" required><br>
+            <label for = "date_naissance">Date de naissance </label><input type = "date" name = "date_naissance" id = "date_naissance" required><br>
+            <label for = "pseudo">Pseudo</label><input type = "text" name = "pseudo" id = "pseudo" required><br>
+            <label for = "email">Email du représentant légal</label><input type = "email" name = "email" id = "email" required><br>
+            <label for = "pass">Mot de passe </label><input type = "password" name = "pass" id = "pass" required/><br>
+            <label for = "pass_verification">Vérification du mot de passe </label><input type = "password" name = "pass_verification" id = "pass_verification" required/><br>
             <button class ="btn btn-primary" type = "submit" >envoyer</button>
             <?php
                 //Message d'erreur si le représentant légale n'as pas mis une bonne date de naissance 
