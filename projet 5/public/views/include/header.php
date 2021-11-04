f63e9e5c2d5b765f477dda844aa8ffc7da2a3e89
michelmi67 <michel.lechevrel@gmail.com>
2021-11-04 18:18:48 +0100

    nouvelle condition

--- projet 5/public/views/include/header.php
+++ projet 5/public/views/include/header.php
@@ -4,10 +4,16 @@
     <div class = "btn_connexion">
         <ul><li><a href = "?action=connexion">Connecte-toi !</a>
                 <a href = "?action=connexion"><img src = "image/connexion.jpg" alt = "image connexion"></a></li>
+                <?php
+                if(isset($_SESSION) AND $_SESSION != null):
+                ?>    
                 <ul>
-                    <li class = "menu_li"><a href = "?action=profil">Profil</a></li>
+                    <li class = "menu_li"><a href = "?action=profil&pseudo=<?php echo $_SESSION['pseudo_enfant'];?>">Profil</a></li>
                     <li class = "menu_li"><a href = "?action=deconnexion">déconnexion</a></li>
                 </ul>
+                <?php
+                endif;
+                ?>
             </li>
         </ul>
     </div>
