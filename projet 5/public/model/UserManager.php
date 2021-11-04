97aeed2e3849728414c3c008eea652ba20d0ed4b
michelmi67 <michel.lechevrel@gmail.com>
2021-11-04 19:36:31 +0100

    changement de nom

--- projet 5/public/model/UserManager.php
+++ projet 5/public/model/UserManager.php
@@ -6,7 +6,7 @@ class UserManager extends Manager
     public function connexion($pseudo_connexion,$pass_connexion)
     {
         $db = $this->dbConnect();
-        $req = $db->prepare('SELECT pseudo_enfant,pass_enfant,rang FROM enfant WHERE pseudo_enfant = ?');
+        $req = $db->prepare('SELECT id,pseudo,pass,rang FROM utilisateur WHERE pseudo = ?');
         $req->execute(array($pseudo_connexion));
         $pseudo = $req->fetch();
         return $pseudo;
@@ -15,7 +15,7 @@ class UserManager extends Manager
     public function enregistrement($nom,$prenom,$date_naissance,$pseudo,$email,$mdp_hache,$rang)
     {
         $db = $this->dbConnect();
-        $req = $db->prepare('INSERT INTO enfant (nom_enfant,prenom_enfant,date_naissance_enfant,pseudo_enfant,email_parent,pass_enfant,rang) VALUES (?,?,?,?,?,?,?)');
+        $req = $db->prepare('INSERT INTO utilisateur (nom,prenom,date_naissance,pseudo,email,pass,rang) VALUES (?,?,?,?,?,?,?)');
         $req->execute(array($nom,$prenom,$date_naissance,$pseudo,$email,$mdp_hache,$rang));
         
     }
