e5c906199c258a16ed8accbc12b241a31e372429
michelmi67 <michel.lechevrel@gmail.com>
2021-11-04 18:17:12 +0100

    modification UserManager

--- projet 5/public/model/UserManager.php
+++ projet 5/public/model/UserManager.php
@@ -3,20 +3,20 @@ require_once("model/Manager.php");
 
 class UserManager extends Manager
 {
-    public function connexion_enfant($email_connexion,$pass_connexion)
+    public function connexion($pseudo_connexion,$pass_connexion)
     {
         $db = $this->dbConnect();
-        $req = $db->prepare('SELECT id,nom,prenom,email,pass FROM admin WHERE email = ?');
-        $req->execute(array($email_connexion));
-        $email = $req->fetch();
-        return $email;
+        $req = $db->prepare('SELECT pseudo_enfant,pass_enfant,rang FROM enfant WHERE pseudo_enfant = ?');
+        $req->execute(array($pseudo_connexion));
+        $pseudo = $req->fetch();
+        return $pseudo;
     }
 
-    public function enregistrement($nom,$prenom,$date_naissance,$pseudo,$email,$mdp_hache)
+    public function enregistrement($nom,$prenom,$date_naissance,$pseudo,$email,$mdp_hache,$rang)
     {
         $db = $this->dbConnect();
-        $req = $db->prepare('INSERT INTO enfant (nom_enfant,prenom_enfant,date_naissance_enfant,pseudo_enfant,email_parent,pass_enfant) VALUES (?,?,?,?,?,?)');
-        $req->execute(array($nom,$prenom,$date_naissance,$pseudo,$email,$mdp_hache));
+        $req = $db->prepare('INSERT INTO enfant (nom_enfant,prenom_enfant,date_naissance_enfant,pseudo_enfant,email_parent,pass_enfant,rang) VALUES (?,?,?,?,?,?,?)');
+        $req->execute(array($nom,$prenom,$date_naissance,$pseudo,$email,$mdp_hache,$rang));
         
     }
 }
