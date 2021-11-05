6eb48c838af4240ce8c4f0dc1bf80daa7553654d
michelmi67 <michel.lechevrel@gmail.com>
2021-11-05 20:06:21 +0100

    ajout article

--- projet 5/public/views/accueil.php
+++ projet 5/public/views/accueil.php
@@ -14,5 +14,21 @@ session_start();
      require('include/nav_left.php'); 
     //inclusion de la barre de navigation
     require('include/nav.php');
-    ?>   
+    ?>
+    <body class = "accueil">
+        <h2 class = "accueil_h2">Actualité</h2>
+        <article>
+            <?php
+            foreach($articles as $article)
+            {
+                ?>
+                <div class = "article">
+                    <p class = "pseudo"><?php  echo $article['pseudo'], ' le ' ,  $article['date_creation_fr']; ?></p> 
+                    <?php echo $article['message'] ?>;
+                </div>
+                <?php
+            }
+            ?>
+        </article>
+    </body>   
 </html>
