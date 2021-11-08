9891e0f4db6ab58764316dc2804b6eaca15ad2d2
michelmi67 <michel.lechevrel@gmail.com>
2021-11-08 20:09:58 +0100

    instanciation de variables

--- projet 5/public/js/main.js
+++ projet 5/public/js/main.js
@@ -17,8 +17,20 @@ let backButton =  document.querySelector("#back");
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext("2d")
 let cleanCanvas = document.getElementById('bt-clear');
-
+let black = document.getElementsByClassName('noir')[0];
+let yellow = document.getElementsByClassName('jaune')[0];
+let red = document.getElementsByClassName('rouge')[0];
+let blue = document.getElementsByClassName('bleu')[0];
+let green = document.getElementsByClassName('vert')[0];
+let pink = document.getElementsByClassName('rose')[0];
+let grey = document.getElementsByClassName('gris')[0];
+let white = document.getElementsByClassName('blanc')[0];
+let violet = document.getElementsByClassName('violet')[0];
+let orange = document.getElementsByClassName('orange')[0];
+let turquoise = document.getElementsByClassName('turquoise')[0];
+let brown = document.getElementsByClassName('brun')[0];
 
 //Instanciation du canvas
-let canvas_1 = new CanvasObjet(canvas,ctx,cleanCanvas);
+let canvas_1 = new CanvasObjet(canvas,ctx,cleanCanvas,black,yellow,red,blue,green,pink,grey,white,violet,orange,turquoise,brown);
 canvas_1.canvasEvent();
+canvas_1.changeColor();
