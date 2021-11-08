668a0402ce08348897da3712bdc9c5c2d213f552
michelmi67 <michel.lechevrel@gmail.com>
2021-11-08 20:13:33 +0100

    supression pour message d'erreur inutile

--- projet 5/public/views/inscription.php
+++ projet 5/public/views/inscription.php
@@ -8,8 +8,7 @@
     <?php require('include/header.php'); ?>
     <body class = "inscription">
         <h1>Inscription</h1>
-        <form method = "post" action = "#">
-            <label for = "date_naissance">Date de naissance du responsable légal</label><input type = "date" name = "date_naissance" id = "date_naissance"><br>
+        <form method = "post" action = "?action=inscription">
             <label for = nom>Nom</label><input type = "text" name = "nom" id = "nom" required><br>
             <label for = "prenom">Prénom</label><input type = "text" name = "prenom" id = "prenom" required><br>
             <label for = "date_naissance">Date de naissance </label><input type = "date" name = "date_naissance" id = "date_naissance" required><br>
@@ -19,11 +18,7 @@
             <label for = "pass_verification">Vérification du mot de passe </label><input type = "password" name = "pass_verification" id = "pass_verification" required/><br>
             <button class ="btn btn-primary" type = "submit" >envoyer</button>
             <?php
-                //Message d'erreur si le représentant légale n'as pas mis une bonne date de naissance 
-                if(isset($erreur))
-                {
-                    echo $erreur;  
-                }
+                
                 //Message d'erreur si les 2 mots de passe ne sont pas les mêmes
                 if(isset($message_erreur))
                 {
