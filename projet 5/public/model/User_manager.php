87b47bddbd1a96fcca383ab99ae5204e4cc07f66
michelmi67 <michel.lechevrel@gmail.com>
2021-11-08 20:11:46 +0100

    ajout de method

--- projet 5/public/model/User_manager.php
+++ projet 5/public/model/User_manager.php
@@ -19,4 +19,26 @@ class User_Manager extends Manager
         $req->execute(array($nom,$prenom,$date_naissance,$pseudo,$email,$mdp_hache,$rang));
         
     }
+
+    public function recup_utilisateur()
+    {
+        $db = $this->dbConnect();
+        $req = $db->query('SELECT id,nom,prenom,DATE_FORMAT(date_naissance,"%d/%m/%Y") AS date_naissance_fr,pseudo,email,rang,
+        DATE_FORMAT(date_creation,"%d/%m/%Y") AS date_creation_fr FROM utilisateur');
+        $utilisateurs = [];
+        while($row = $req->fetch())
+        {
+            $utilisateurs[] = $row;
+        }
+        $req->CloseCursor();
+        return $utilisateurs;
+    }
+
+    /*public function admin()
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('UPDATE utilisateur SET rang = ?');
+        $req->execute(array($_SESSION ='1'));
+
+    }*/
 }
