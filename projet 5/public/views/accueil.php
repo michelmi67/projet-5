02ba9fae8b5433d66470a7ad80aa8b28ef4e89fc
michelmi67 <michel.lechevrel@gmail.com>
2021-11-08 20:14:25 +0100

    securité de la page + nouveu lien

--- projet 5/public/views/accueil.php
+++ projet 5/public/views/accueil.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+if(!$_SESSION){
+    header('Location:?action=bienvenu');
+}
+
 ?>
 <!DOCTYPE HTML>
 <html lang = "fr">
@@ -8,13 +12,19 @@ session_start();
         <!--inclusion du head-->
         <?php require('include/head.php'); ?>
     </head>
-    <!--inclusion du header-->
-    <?php require('include/header.php'); 
+    <header>
+        <!--inclusion du header-->
+        <?php require('include/header.php');?>
+        <script src="https://kit.fontawesome.com/2e63600e57.js" crossorigin="anonymous"></script> 
+    </header>
+    <?php
     //inclusion de la barre de navigation gauche
      require('include/nav_left.php'); 
     //inclusion de la barre de navigation
     require('include/nav.php');
     ?>
+    
+
     <body class = "accueil">
         <h2 class = "accueil_h2">Actualité</h2>
         <article>
@@ -23,8 +33,9 @@ session_start();
             {
                 ?>
                 <div class = "article">
-                    <p class = "pseudo"><?php  echo $article['pseudo'], ' le ' ,  $article['date_creation_fr']; ?></p> 
-                    <?php echo $article['message'] ?>;
+                    <a href = "?action=article&article_id=<?php echo $article['id']; ?>"><p class = "pseudo"><?php  echo $article['pseudo'], ' le ' ,  $article['date_creation_fr']; ?></p></a> 
+                    <?php echo $article['message'] ?>
+                    <a href = "?action=article&article_id=<?php echo $article['id']; ?>"><i class="fas fa-comment"></i>commentaire</a>
                 </div>
                 <?php
             }
