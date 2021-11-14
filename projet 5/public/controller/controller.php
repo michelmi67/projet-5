59b53862406ee9dd90084289a1a8a198f17e2fe2
michelmi67 <michel.lechevrel@gmail.com>
2021-11-14 20:03:29 +0100

    ajout de method + commentaires

--- projet 5/public/controller/controller.php
+++ projet 5/public/controller/controller.php
@@ -10,6 +10,8 @@ class Controller
     {
         require('views/bienvenu.php');
     }
+
+    //récupération des  articles sur la page accueil
     public function accueil()
     {
         $post_manager = new Post_Manager();
@@ -17,6 +19,7 @@ class Controller
         require('views/accueil.php');
     }
 
+    //connexion d'un utilisateur
     public function connexion(){
     session_start();
  
@@ -62,12 +65,12 @@ class Controller
     require('views/connexion.php');
     }
 
+    //inscription d'un nouvel utilisateur par défaut rang 3 (utilisateur)
     public function inscription(){
     $userManager = new User_Manager();
     if($_POST){
-        //Inscription d'un utilisateur
         
-        //Instanciation et protection des données
+        //Instanciation des variables et protection des données
         $nom = htmlspecialchars($_POST['nom']) ;
         $prenom = htmlspecialchars($_POST['prenom']);
         $date_naissance = $_POST['date_naissance'];
@@ -83,8 +86,8 @@ class Controller
             //on hache le mot de passe
             $mdp_hache = password_hash($mdp, PASSWORD_DEFAULT);
 
-            //Puis on créer un nouvel enfant
-            $enfant = $userManager->enregistrement($nom,$prenom,$date_naissance,$pseudo,$email,$mdp_hache,$rang);
+            //Puis on créer un nouvel utilisateur
+            $utilasteur = $userManager->enregistrement($nom,$prenom,$date_naissance,$pseudo,$email,$mdp_hache,$rang);
             header('Location:?action=accueil');   
         }
         else
@@ -96,6 +99,7 @@ class Controller
     require('views/inscription.php');
     }
 
+    //deconexion d'un utilisateur
     public function deconnexion(){
         
         session_start();
@@ -105,23 +109,66 @@ class Controller
     }
 
     public function profil(){
+        session_start();
+        $post_manager = new Post_Manager();
+        $pseudo = $_SESSION['pseudo'];
+        $signaler = 'false';
+        //recupération des articles dans profil
+        $articles_profil = $post_manager->recup_posts_profil($pseudo);
         if($_POST)
         {   
-            $pseudo = $_GET['pseudo'];
-            $post_manager = new Post_Manager();
             $message = $_POST['message'];
-            $post_manager->envoi_post($pseudo,$message);
-            header('Location:?action=accueil');
-            $articles = $post_manager->recup_post_profil($pseudo);
+            //envoi d'un article dans la page accueil
+            $post_manager->envoi_post($pseudo,$message,$signaler);
+            header('Location:?action=accueil');    
         }
         require("views/profil.php");    
     }
 
+    //récupération des utilisateur 
     public function utilisateur(){
         $userManager = new User_Manager();
         $utilisateurs = $userManager->recup_utilisateur();
-        /*$admin = $userManager->admin();*/
         require('views/utilisateur.php');
+    }
+
+    //creer un administrateur
+    public function creer_admin()
+    {
+        $userManager = new User_Manager();
+        $rang = '1';
+        $pseudo = $_GET['pseudo'];
+        $admin = $userManager->rang($rang,$pseudo);
+        header('location:?action=utilisateur');
+    }
+
+    //creer un modérateur
+    public function creer_moderateur()
+    {
+        $userManager = new User_Manager();
+        $rang = '2';
+        $pseudo = $_GET['pseudo'];
+        $moderateur = $userManager->rang($rang,$pseudo);
+        header('location:?action=utilisateur');
+    }
+
+    //creer un utilisateur (controle admin)
+    public function creer_utilisateur()
+    {
+        $userManager = new User_Manager();
+        $rang = '3';
+        $pseudo = $_GET['pseudo'];
+        $utilisateur = $userManager->rang($rang,$pseudo);
+        header('location:?action=utilisateur');
+    }
+
+    //suprimer un utilisateur
+    public function suprime_utilisateur()
+    {
+        $userManager = new User_Manager();
+        $pseudo = $_GET['pseudo'];
+        $moderateur = $userManager->delete_user($pseudo);
+        header('location:?action=utilisateur');
     }
 
     public function video(){
@@ -132,20 +179,71 @@ class Controller
         require('views/dessin.php');
     }
 
+    //Récuperer un article et ses commentaires
+
+    //recupérer un article
     public function article()
     {
+        session_start();
         $post_manager = new Post_Manager();
         $comment_manager = new Comment_Manager();
-        $id = $_GET['article_id'];
+        $id = (int)$_GET['id'];
         $article = $post_manager->recup_post($id);
+        
+        //Inserer un commmentaire
         if($_POST)
         {
+            $auteur = $_SESSION['pseudo'];
+            $message = $_POST['commentaire'];
             $signaler = 'false';
-            $auteur = "michel";
-            $message = $_POST['message'];
-            $comments = $comment_manager->insert_comment($id,$auteur,$message,$signaler);
-
+            $comment_manager->insert_comment($id,$auteur,$message,$signaler);
+            
         }
+
+        //récupération des commentaires
+        $commentaires = $comment_manager->recup_comments($id);
+
         require('views/article.php');
     }
+
+    //signaler un commentaire
+    public function signal_comment()
+    {
+        $comment_manager = new Comment_Manager();
+        $signaler = 'true';
+        $id = $_POST['commentaire'];
+        $comment_manager->signaler_commentaire($signaler,$id);
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+    }
+
+    //récupération des articles (admin, modérateur)
+    public function messages()
+    {
+        $post_manager = new Post_Manager();
+        $signaler = 'false';
+        $articles = $post_manager->recup_all_posts($signaler);
+        $signaler = 'true';
+        $articles_signalés = $post_manager->recup_all_posts_signalés($signaler);
+        require('views/messages.php');
+    }
+
+    //supression d'un article 
+    public function suprime_article()
+    {
+        $post_manager = new Post_Manager();
+        $id = $_GET['id'];
+        $post_manager->delete_article($id);
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+    }
+
+    //Signalement d'un article dans la page d'accueil
+    public function signal_article_user()
+    {
+        $post_manager = new Post_Manager();
+        $signaler = "true";
+        $id = $_GET['id'];
+        $post_manager->signaler_article_utilisateur($signaler,$id);
+        header('Location:?action=accueil');
+    }
+
 }
