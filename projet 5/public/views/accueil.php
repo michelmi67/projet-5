04268672716da5145e0e5436ad41e2d93527e46b
michelmi67 <michel.lechevrel@gmail.com>
2021-11-14 20:06:24 +0100

    ajout de fonctionalités

--- projet 5/public/views/accueil.php
+++ projet 5/public/views/accueil.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
-
 if(!$_SESSION){
     header('Location:?action=bienvenu');
 }
-
 ?>
 <!DOCTYPE HTML>
 <html lang = "fr">
@@ -23,8 +21,6 @@ if(!$_SESSION){
     //inclusion de la barre de navigation
     require('include/nav.php');
     ?>
-    
-
     <body class = "accueil">
         <h2 class = "accueil_h2">Actualité</h2>
         <article>
@@ -32,10 +28,28 @@ if(!$_SESSION){
             foreach($articles as $article)
             {
                 ?>
-                <div class = "article">
-                    <a href = "?action=article&article_id=<?php echo $article['id']; ?>"><p class = "pseudo"><?php  echo $article['pseudo'], ' le ' ,  $article['date_creation_fr']; ?></p></a> 
+                <div class = "article" id="article_<?php echo $article['id']; ?>">
+                    <a href = "?action=article&id=<?php echo $article['id']; ?>"><p class = "pseudo"><?php  echo $article['pseudo'], ' le ' ,  $article['date_creation_fr']; ?></p></a> 
                     <?php echo $article['message'] ?>
-                    <a href = "?action=article&article_id=<?php echo $article['id']; ?>"><i class="fas fa-comment"></i>commentaire</a>
+                    <a href = "?action=article&id=<?php echo $article['id']; ?>"><i class="fas fa-comment"></i>commentaire</a>
+                    <?php
+                    // si un utilisateur veut signaler un commentaire
+                    if($_SESSION['rang'] === '3')
+                    {
+                        ?>
+                        <a href = "?action=signal_article_user&id=<?php echo $article['id']; ?>"><i class="fas fa-exclamation-triangle"></i>signaler</a>
+                        <?php
+                    }
+                    ?>
+                    <?php
+                    //Si l'auteur veut supprimer son commentaire 
+                    if($_SESSION['pseudo'] === $article['pseudo'] OR $_SESSION['rang'] == '1' OR $_SESSION['rang'] == '2')
+                    {
+                        ?>
+                        <a href = "?action=suprime_article&id=<?php echo $article['id']; ?>"><i class="fas fa-times"></i>supprimer</a>
+                        <?php
+                    }
+                    ?>
                 </div>
                 <?php
             }
