fa4195da01da01882042331dd2263c121a930a44
michelmi67 <michel.lechevrel@gmail.com>
2021-11-14 20:06:57 +0100

    récupération des commentaires

--- projet 5/public/views/article.php
+++ projet 5/public/views/article.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 
 if(!$_SESSION){
     header('Location:?action=bienvenu');
@@ -12,12 +11,17 @@ if(!$_SESSION){
         <?php require('include/head.php'); ?>
     </head>
     <!--inclusion du header-->
-    <?php require('include/header.php'); 
+    <header>
+        <?php require('include/header.php'); ?>
+        <script src="https://kit.fontawesome.com/2e63600e57.js" crossorigin="anonymous"></script>
+    </header>
+    <?php 
     //inclusion de la barre de navigation gauche
      require('include/nav_left.php'); 
     //inclusion de la barre de navigation
     require('include/nav.php');
     ?>
+    <!-- Article -->
     <body class = "page_article">
         <article>
             <div class = "article">
@@ -25,10 +29,36 @@ if(!$_SESSION){
                 <?php echo $article['message'] ?>
             </div>
         </article>
-        <form method = "post" action = "?action=article&article_id=<?php echo $article['id'] ?>">
-        <textarea name = "message" placeholder = "ton commentaire"rows = "6" cols = "75" required></textarea><br>
-        <button class = "btn-primary" type = "submit">Envoyer</button>
+
+        <!-- Envoie d'un commentaire -->
+        <form method = "post" action = "?action=article&id=<?php echo $article['id'] ?>">
+            <textarea name = "commentaire" placeholder = "ton commentaire"rows = "6" cols = "75" required></textarea><br>
+            <button class = "btn-primary" type = "submit">Envoyer</button>
         </form>
+
+        <!-- Récupération des commentaire -->
+        <?php
+        foreach($commentaires as $commentaire)
+        {
+            ?>
+            <div class="commentaire">
+                <p class = "pseudo"><?php echo $commentaire['auteur'], ' le ', $commentaire['date_creation_fr'] ?></p>
+                <p><?php echo $commentaire['message']; ?></p>
+                
+                    <a href = "?action=signal_comment"><i class="fas fa-exclamation-triangle"></i>signaler</a>
+                    <?php
+                    //Si l'auteur veut supprimer son commentaire 
+                    if($_SESSION['pseudo'] === $commentaire['auteur'])
+                    {
+                        ?>
+                        <i class="fas fa-times"></i>
+                        <?php
+                    }
+                    ?>
+            </div>
+            <?php
+        }
+        ?>
          
     </body>
 </html>
