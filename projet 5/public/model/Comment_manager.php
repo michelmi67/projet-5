ea7f649290dff41b88cffcbadab8c853d029d48a
michelmi67 <michel.lechevrel@gmail.com>
2021-11-14 20:04:54 +0100

    ajout method

--- projet 5/public/model/Comment_manager.php
+++ projet 5/public/model/Comment_manager.php
@@ -6,7 +6,26 @@ class Comment_Manager extends Manager
     public function insert_comment($id,$auteur,$message,$signaler)
     {
         $db = $this->dbConnect();
-        $req = $db->prepare('INSERT INTO commentaire (id_page,auteur,message,signaler) VALUES(?,?,?,?');
+        $req = $db->prepare('INSERT INTO commentaire (id_page,auteur,message,signaler) VALUES(?,?,?,?)');
         $req->execute(array($id,$auteur,$message,$signaler));
-    } 
+    }
+    
+    public function recup_comments($id){
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id,auteur,message,DATE_FORMAT(date_creation,"%d/%m/%Y") AS date_creation_fr FROM commentaire WHERE id_page = ?');
+        $req->execute(array($id));
+        $commentaires = [];
+        while($row = $req->fetch())
+        {
+            $commentaires[] = $row;
+        }
+        return $commentaires;
+    }
+
+    public function signaler_commentaire($signaler,$id)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('UPDATE commentaire SET signaler = ? WHERE id = ?');
+        $req->execute(array($signaler,$id));
+    }
 }
--- projet 5/public/model/Post_manager.php
+++ projet 5/public/model/Post_manager.php
@@ -3,17 +3,17 @@ require_once("model/Manager.php");
 
 class Post_Manager extends Manager
 {
-    public function envoi_post($pseudo,$message)
+    public function envoi_post($pseudo,$message,$signaler)
     {
         $db = $this->dbConnect();
-        $req = $db->prepare('INSERT INTO article (pseudo,message) VALUES (?,?)');
-        $req->execute(array($pseudo,$message));
+        $req = $db->prepare('INSERT INTO article (pseudo,message,signaler) VALUES (?,?,?)');
+        $req->execute(array($pseudo,$message,$signaler));
     }
 
     public function recup_posts()
     {
         $db = $this->dbConnect();
-        $req = $db->query('SELECT id,pseudo,message,DATE_FORMAT(date_creation,"%d/%m/%Y") AS date_creation_fr FROM article ORDER BY date_creation DESC');
+        $req = $db->query('SELECT id,pseudo,message,signaler,DATE_FORMAT(date_creation,"%d/%m/%Y") AS date_creation_fr FROM article ORDER BY date_creation DESC');
         $articles = [];
         while($row = $req->fetch())
         {
@@ -23,18 +23,18 @@ class Post_Manager extends Manager
         return $articles;
     }
 
-    public function recup_post_profil($pseudo)
+    public function recup_posts_profil($pseudo)
     {
         $db = $this->dbConnect();
-        $req = $db->prepare('SELECT pseudo,message,DATE_FORMAT(date_creation,"%d/%m/%Y") AS date_creation_fr FROM article WHERE pseudo = ? ORDER BY date_creation DESC');
+        $req = $db->prepare('SELECT pseudo,message,signaler,DATE_FORMAT(date_creation,"%d/%m/%Y") AS date_creation_fr FROM article WHERE pseudo = ? ORDER BY date_creation DESC');
         $req->execute(array($pseudo));
-        $articles = [];
+        $articles_profil = [];
         while($row = $req->fetch())
         {
-            $articles[] = $row;
+            $articles_profil[] = $row;
         }
         $req->CloseCursor();
-        return $articles;
+        return $articles_profil;
     }
 
     public function recup_post($id)
@@ -44,6 +44,47 @@ class Post_Manager extends Manager
         $req->execute(array($id));
         $article = $req->fetch();
         return $article; 
+    }
+
+    public function recup_all_posts($signaler)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id,pseudo,message,signaler,DATE_FORMAT(date_creation,"%d/%m/%Y") AS date_creation_fr FROM article WHERE signaler = ? ORDER BY date_creation DESC');
+        $req->execute(array($signaler));
+        $articles_profil = [];
+        while($row = $req->fetch())
+        {
+            $articles[] = $row;
+        }
+        $req->CloseCursor();
+        return $articles;
+    }
+
+    public function recup_all_posts_signalés($signaler)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id,pseudo,message,signaler,DATE_FORMAT(date_creation,"%d/%m/%Y") AS date_creation_fr FROM article WHERE signaler = ? ORDER BY date_creation DESC');
+        $req->execute(array($signaler));
+        $articles_signalés = [];
+        while($row = $req->fetch())
+        {
+            $articles_signalés[] = $row;
+        }
+        $req->CloseCursor();
+        return $articles_signalés;
+    }
+
+    public function delete_article($id)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('DELETE FROM article WHERE id = ?');
+        $req->execute(array($id));
+    }
+    public function signaler_article_utilisateur($signaler,$id)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('UPDATE article SET signaler = ? WHERE id = ?');
+        $req->execute(array($signaler,$id));
     }
 
 }
