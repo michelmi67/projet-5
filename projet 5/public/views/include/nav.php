f700658c76169226889b40407b6b1b3fb4a925c9
michelmi67 <michel.lechevrel@gmail.com>
2021-11-29 01:04:31 +0100

    changement de lien

--- projet 5/public/views/include/nav.php
+++ projet 5/public/views/include/nav.php
@@ -4,7 +4,7 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="?action=accueil">Accueil <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link active" href="?action=accueil&page=1">Accueil <span class="sr-only">(current)</span></a>
         <a class="nav-item nav-link" href="?action=video">vidéos<span class="sr-only">(current)</span></a>
         <a class="nav-item nav-link" href="?action=dessin">dessins<span class="sr-only">(current)</span></a>
         <a class="nav-item nav-link" href="?action=jeux">Jeux<span class="sr-only">(current)</span></a>
